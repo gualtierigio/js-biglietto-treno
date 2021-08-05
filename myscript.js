275198574d875b01e76a4259273a352ccc3241f4
gualtierigio <gualtierigio@gmail.com>
2021-08-05 17:09:51 +0200

    ultima modifica

--- myscript.js
+++ myscript.js
@@ -4,6 +4,19 @@ let Age = prompt("Quanti anni hai?");
 
 const KmPrice = (0,21);
 
-let TicketPrice = (Km * KmPrice);
+let TicketPrice = Km * KmPrice;
 
-console.log(TicketPrice);
+const ScontoVenti = (TicketPrice * 20) / 100;
+
+const ScontoQuaranta = (TicketPrice * 40) / 100;
+
+if (Age < 18) {
+    FinalPrice = TicketPrice - ScontoVenti; 
+}
+else if (Age > 65) {
+    FinalPrice = TicketPrice - ScontoQuaranta;
+}
+
+console.log(FinalPrice)
+
+document.getElementById(FinalPrice).innerHTML
